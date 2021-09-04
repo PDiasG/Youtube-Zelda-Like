7c466a244c84b5cae53776ce73b17f4d40ce71e1
Pedro Dias Garcia <pdiasg@gmail.com>
2021-09-03 20:44:52 -0700

    Setup + Player Movement & Animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidbody2d;
+    private Vector3 change;
+    private Animator animator;
+
+    void Start() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate() {
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove() {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter() {
+        rigidbody2d.MovePosition(transform.position + change * speed * Time.fixedDeltaTime);
+    }
+}
